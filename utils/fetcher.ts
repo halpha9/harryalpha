93cc62d8d66975a285c10764b8d70e87f0c46700
halpha9 <harryalpha@hotmail.co.uk>
2022-12-19 20:35:37 +0000

    prop safety

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -7,10 +7,7 @@ import {
 } from "../types/sanity";
 
 // const baseUrl = process.env.NEXT_PUBLIC_PRODUCTION_URL!;
-const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_PRODUCTION_URL!
-    : "http://localhost:3000";
+const baseUrl = "http://localhost:3000";
 
 export const fetchSkills = async (): Promise<SkillsSanity[]> => {
   const response = await fetch(`${baseUrl}/api/getSkills`);
